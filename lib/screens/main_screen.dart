c8def3d3f7fa146045538eba9ebea0a5c1e537f8
Atuoha <atuohainitiatives@gmail.com>
2022-06-27 07:09:55 +0100

    main weather screen dynamic location switching

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -6,6 +6,7 @@ import 'package:weatherapp/providers/city.dart';
 import 'package:http/http.dart' as http;
 
 import '../constants/constants.dart';
+import 'package:intl/intl.dart';
 
 class MainScreen extends StatefulWidget {
   static const routeName = '/home';
@@ -36,25 +37,28 @@ class _MainScreenState extends State<MainScreen> {
       var searchedResult = await http.get(Uri.parse(urlString));
       var response = json.decode(searchedResult.body);
 
-      // response sample
+      // RESPONSE SAMPLE
       // {"coord":{"lon":7.4943,"lat":6.4402},"weather":[{"id":500,"main":"Rain","description":"light rain","icon":"10n"}],"base":"stations","main":{"temp":297.79,"feels_like":298.63,"temp_min":297.79,"temp_max":297.79,"pressure":1011,"humidity":89,"sea_level":1011,"grnd_level":988},"visibility":10000,"wind":{"speed":2.46,"deg":221,"gust":6.35},"rain":{"1h":0.11},"clouds":{"all":100},"dt":1656094652,"sys":{"country":"NG","sunrise":1656047850,"sunset":1656092832},"timezone":3600,"id":2343279,"name":"Enugu","cod":200}
-      print(cityData.weatherCity);
-      print(response['weather'][0]['description']); //description
-      print(response['weather'][0]['main']); //weather
-      print(response['weather'][0]['icon']); //icon
-      print(response['main']['temp']); //temp
-      print(response['main']['feels_like']); //feels like
-      print(response['main']['temp_min']); //min_temp
-      print(response['main']['temp_max']); //max_temp
-      print(response['main']['pressure']); //pressure
-      print(response['main']['humidity']); //humidity
-      print(response['wind']['speed']); //windSpeed
-      print(response['wind']['deg']); //wind Degree
-      print(response['sys']['country']); //country
-      print(response['sys']['sunrise']); //sunrise
-      print(response['sys']['sunset']); //sunset
-      print(response['timezone']); //timezone
 
+      // RESPONSE DISTRIBUTION
+      // print(cityData.weatherCity);
+      // print(response['weather'][0]['description']); //description
+      // print(response['weather'][0]['main']); //weather
+      // print(response['weather'][0]['icon']); //icon
+      // print(response['main']['temp']); //temp
+      // print(response['main']['feels_like']); //feels like
+      // print(response['main']['temp_min']); //min_temp
+      // print(response['main']['temp_max']); //max_temp
+      // print(response['main']['pressure']); //pressure
+      // print(response['main']['humidity']); //humidity
+      // print(response['wind']['speed']); //windSpeed
+      // print(response['wind']['deg']); //wind Degree
+      // print(response['sys']['country']); //country
+      // print(response['sys']['sunrise']); //sunrise
+      // print(response['sys']['sunset']); //sunset
+      // print(response['timezone']); //timezone
+
+      // Updating new weather details through provider
       cityData.updateWeather(
           // You can assign them to a variable if you desire
           response['main']['humidity'],
@@ -69,7 +73,8 @@ class _MainScreenState extends State<MainScreen> {
           response['main']['temp_min'],
           response['weather'][0]['main'],
           response['main']['feels_like'],
-          'http://openweathermap.org/img/wn/${response['weather'][0]['icon'] + '@3x.png'}',
+          // 'http://openweathermap.org/img/wn/${response['weather'][0]['icon'] + '@3x.png'}',
+          response['weather'][0]['icon'],
           response['weather'][0]['description'],
           response['weather'][0]['main'],
           response['sys']['country']);
@@ -92,9 +97,11 @@ class _MainScreenState extends State<MainScreen> {
     );
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Constants.primaryColor,
+        // backgroundColor: Constants.primaryColor,
+        backgroundColor: Colors.white,
         automaticallyImplyLeading: false,
         titleSpacing: 0,
+        elevation: 0,
         title: Container(
           padding: const EdgeInsets.symmetric(horizontal: 20),
           width: size.width,
@@ -110,34 +117,44 @@ class _MainScreenState extends State<MainScreen> {
               cityData.selectionMode
                   ? Row(
                       children: [
-                        const Icon(Icons.pin_drop, color: Colors.white),
-                        DropdownButton(
-                          borderRadius: BorderRadius.circular(8),
-                          dropdownColor: Constants.primaryColor,
-                          icon: const Icon(
-                            Icons.keyboard_arrow_down,
-                            color: Colors.white,
-                          ),
-                          value: cityData.weatherCity,
-                          items: cityData
-                              .getSelectedCities()
-                              .map(
-                                (data) => DropdownMenuItem(
-                                  value: data.city,
-                                  child: Text(
-                                    data.city,
-                                    style: const TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold,
+                        Image.asset(
+                          'assets/images/pin.png',
+                          color: Constants.primaryColor,
+                          width: 20,
+                        ),
+                        const SizedBox(width: 5),
+                        DropdownButtonHideUnderline(
+                          child: DropdownButton(
+                            borderRadius: BorderRadius.circular(8),
+                            // dropdownColor: Constants.primaryColor,
+                            icon: const Icon(
+                              Icons.keyboard_arrow_down,
+                              color: Colors.black,
+                            ),
+                            value: cityData.weatherCity,
+                            items: cityData
+                                .getSelectedCities()
+                                .map(
+                                  (data) => DropdownMenuItem(
+                                    value: data.city,
+                                    child: Text(
+                                      data.city,
+                                      style: const TextStyle(
+                                        // color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
                                     ),
                                   ),
-                                ),
-                              )
-                              .toList(),
-                          onChanged: (value) {
-                            cityData.updateWeatherCity(value.toString());
-                            print(value.toString());
-                          },
+                                )
+                                .toList(),
+                            onChanged: (value) {
+                              cityData.updateWeatherCity(value.toString());
+                              setState(() {
+                                isInit = true;
+                              });
+                              print('City:$value');
+                            },
+                          ),
                         )
                       ],
                     )
@@ -153,15 +170,103 @@ class _MainScreenState extends State<MainScreen> {
                             fontWeight: FontWeight.bold,
                             fontSize: 17,
                           ),
-                        )
+                        ),
                       ],
                     )
             ],
           ),
         ),
       ),
-      body: Column(
-        children: [],
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              cityData.weatherCity,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+              ),
+            ),
+            Text(
+              DateFormat.yMMMMEEEEd().format(
+                DateTime.now(),
+              ),
+              style: const TextStyle(
+                fontSize: 15,
+                color: Colors.grey,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Container(
+              width: size.width,
+              height: 200,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Constants.primaryColor,
+                boxShadow: [
+                  BoxShadow(
+                    color: Constants.primaryColor.withOpacity(.5),
+                    offset: const Offset(0, 25),
+                    blurRadius: 10,
+                    spreadRadius: -12,
+                  ),
+                ],
+              ),
+              child: Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  Positioned(
+                    top: -40,
+                    right: 20,
+                    child: Image.asset(
+                      'assets/images/${cityData.weatherImgUrl}.png',
+                      width: 150,
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 30,
+                    right: 20,
+                    child: Text(
+                      cityData.weatherWeatherStateName,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 50,
+                    left: 10,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          cityData.weatherTemp.toStringAsFixed(0),
+                          style: const TextStyle(
+                            fontSize: 80,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white38,
+                          ),
+                        ),
+                        const Text(
+                          'o',
+                          style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white38,
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
